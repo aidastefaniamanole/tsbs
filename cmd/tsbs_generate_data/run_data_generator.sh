cd6d78a55ba0cc8df444655fe1bf7362cf904ccf
Aida - Stefania Manole <aida-stefania.manole@campus.tu-berlin.de>
2022-01-27 11:15:11 +0100

    updated the data generator script

--- cmd/tsbs_generate_data/run_data_generator.sh
+++ cmd/tsbs_generate_data/run_data_generator.sh
@@ -4,26 +4,32 @@
 OUTPUT_DIR="/home/stefania/IdeaProjects/vm-benchmarking-client/src/main/resources"
 USE_CASE="devops"
 SCALE=${SCALE:-"8"}
-
-# generate the 14 days old data
-TIMESTAMP_START=$(date +"%Y-%m-%dT%H:%M:%S%:z" -d "2 days ago")
-TIMESTAMP_END=$(date +"%Y-%m-%dT%H:%M:%S%:z")
+LOG_INTERVAL=${LOG_INTERVAL:-"10"}
 
 TIMESTAMP_START=$(date --utc +%FT%TZ -d "2 days ago")
-TIMESTAMP_END=$(date --utc +%FT%TZ)
+if [[ $1 == "days" ]]; then
+  echo "Generating data with starting timestamp from $2 day(s) ago"
+  TIMESTAMP_START=$(date --utc +%FT%TZ -d "$2 days ago")
+  TIMESTAMP_END=$(date --utc +%FT%TZ)
+  DIFF=$(( $(date +%s -d ${TIMESTAMP_END})-$(date +%s -d ${TIMESTAMP_START}) ))
+  EXPERIMENT_SPAN=$(( $DIFF / (60 * 60 * 24) )) # to get the number of days the data spans over
+else
+  echo "Generating data with starting timestamp from $2 hour(s) ago"
+  TIMESTAMP_START=$(date --utc +%FT%TZ -d "$2 hours ago")
+  TIMESTAMP_END=$(date --utc +%FT%TZ)
+  DIFF=$(( $(date +%s -d ${TIMESTAMP_END})-$(date +%s -d ${TIMESTAMP_START}) ))
+  EXPERIMENT_SPAN=$(( $DIFF / (60 * 60) )) # to get the number of hours the data spans over
+fi
 
-LOG_INTERVAL=${LOG_INTERVAL:-"10"}
-DIFF=$(( $(date +%s -d ${TIMESTAMP_END})-$(date +%s -d ${TIMESTAMP_START}) ))
-EXPERIMENT_DAYS=$(( $DIFF / (60 * 60 * 24) )) # to get the number of days the data spans over
 
 echo -e "Use case=${USE_CASE}
 Number of hosts that emit telemetry data=${SCALE}
 Timestamp start=${TIMESTAMP_START}
 Timestamp end=${TIMESTAMP_END}
 The interval data is being logged at in seconds=${LOG_INTERVAL}
-Data span in days=${EXPERIMENT_DAYS}" | tee -a "${OUTPUT_DIR}/${SCALE}hosts_${EXPERIMENT_DAYS}d"
+Data span=${EXPERIMENT_SPAN}$1" | tee -a "${OUTPUT_DIR}/${SCALE}hosts"
 
 go run main.go --use-case=${USE_CASE} --scale=${SCALE} \
      --timestamp-start=${TIMESTAMP_START} \
      --timestamp-end=${TIMESTAMP_END} \
-     --log-interval=${LOG_INTERVAL}s --format="victoriametrics" >> "${OUTPUT_DIR}/${SCALE}hosts_${EXPERIMENT_DAYS}d"
+     --log-interval=${LOG_INTERVAL}s --format="victoriametrics" >> "${OUTPUT_DIR}/${SCALE}hosts"
